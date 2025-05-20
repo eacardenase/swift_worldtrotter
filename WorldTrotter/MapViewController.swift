8bba388d67a14fd1ccdfef30b486bf792bacf068
Edwin Cardenas <ecardenas@lwolf.com>
2025-05-20 16:06:56 -0500

    feat: add map view controller

---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Edwin Cardenas on 5/20/25.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    override func loadView() {
+        view = MKMapView()
+    }
+}
